6680660fa6669d364080d1721d96e640ffba9172
coinables <coinables@gmail.com>
2016-05-21 22:45:47 -0700

    global var
    change global to local var

--- js/price.js
+++ js/price.js
@@ -3,7 +3,7 @@ xbtc.open('GET', 'https://api.bitcoinaverage.com/ticker/global/USD/', true);
 xbtc.onreadystatechange = function(){
 	if(xbtc.readyState == 4){
 		var ticker = JSON.parse(xbtc.responseText);
-		price = ticker.last;
+		var price = ticker.last;
 		document.getElementById('btc').innerHTML = "$" + price;
 	}
 };
